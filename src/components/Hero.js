c3da9fbc34287e5f49649fce864b8711e2ab6c76
yeenathan <yeenathan21@gmail.com>
2025-01-09 14:35:48 -0800

    icons

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,22 @@
 import { Link } from "react-router";
 
-const RESUME_LINK = "https://www.dropbox.com/scl/fi/t48hatvpkncpldqrotkg0/resume2.pdf?rlkey=7ljnvzola3nqgguggjn72gl57&st=bbyxgc24&dl=1";
+function Icon({url, src, hover}) {
+  return(
+    <Link className="min-w-16" to={url} target="_blank">
+      <img 
+        src={src}
+        onMouseOver={(e) => e.currentTarget.src=hover}
+        onMouseOut={(e) => e.currentTarget.src=src}
+      />
+    </Link>
+  )
+}
 
 function Info() {
   return(
-    <div className="flex flex-col md:flex-row items-center md:gap-8">
-      <a href={RESUME_LINK} download={"resume"}>Resume (PDF)</a>
-      <Link to={"https://github.com/yeenathan/"} target="_blank">Github</Link>
-      <Link to={"https://www.linkedin.com/in/yeenathan/"} target="_blank">LinkedIn</Link>
-      <p>yeenathan21@gmail.com</p>
+    <div className="flex flex-col md:flex-row items-center gap-2 md:gap-4">
+      <Icon url="https://github.com/yeenathan/" src="/images/icons/gh-black.svg" hover="/images/icons/gh-brightblue.svg"/>
+      <Icon url="https://linkedin.com/in/yeenathan/" src="/images/icons/linkedin-black.svg" hover="/images/icons/linkedin-brightblue.svg"/>
     </div>
   )
 }
@@ -18,9 +26,9 @@ export default function Hero({scroll}) {
     <div style={{minHeight: "75vh"}} className="flex flex-col items-center md:items-start min-w-full justify-center gap-2 md:gap-4">
       <div>
         <h1 className="text-5xl md:text-6xl">Nathan Yee</h1>
-        <p className="text-2xl md:text-2xl">Frontend Developer/Designer</p>
+        <p className="text-2xl md:text-2xl">Frontend Developer & Designer</p>
       </div>
-      <div>
+      <div className="flex flex-col gap-4">
         <p className="text-xl text-center md:text-left">Vancouver based frontend developer & visual designer dedicated to providing sophisticated and empathetic solutions.</p>
         <Info/>
       </div>
