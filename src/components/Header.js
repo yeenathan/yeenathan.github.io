6d7b57046b7e05170858c25ba826337386221da8
Nathan <yeenathan21@gmail.com>
2025-01-13 15:29:58 -0800

    new logo final fr this time

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return(
     <div className="flex justify-start w-full items-center">
       <Link className="min-w-12 md:min-w-16 mb-4 md:mb-16" to={"/"}>
-        <img src="/images/logo/logo-v1f.svg"/>
+        <img src="/images/logo/logo-v1f-v2.svg"/>
       </Link>
     </div>
   )
