ea29183acfcd148684d4c446667b58c30813d44d
yeenathan <yeenathan21@gmail.com>
2025-01-15 16:10:02 -0800

    typo fix

--- src/About.js
+++ src/About.js
@@ -12,7 +12,7 @@ export default function About() {
           <img src="/images/logo/logo-final.svg" className="col-start-2 col-span-3"/>
           <div className="flex flex-col items-start col-start-6 col-end-11 gap-2 md:gap-3">
             <h1 className="text-5xl md:text-8xl">About</h1>
-            <p className="text-justify">Hi! I'm Nathan, a front-end developer, UI/UX designer, and graphic designer based in Vancouver. I pride myself on my abiliteis to learn fast and adapt quickly to our ever-changing landscape, while not forgetting where we come from.</p>
+            <p className="text-justify">Hi! I'm Nathan, a front-end developer, UI/UX designer, and graphic designer based in Vancouver. I pride myself on my abilities to learn fast and adapt quickly to our ever-changing landscape, while not forgetting where we come from.</p>
             <p>I really love to try different foods, experiences, and games. Shoot me a recommendation!</p>
           </div>
         </div>
