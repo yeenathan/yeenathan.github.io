06f9a42395c255e0cf92f4e2e27db28b5374b623
yeenathan <yeenathan21@gmail.com>
2026-08-29 02:14:27 -0700

    styles build

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 git checkout gh-pages && \
 git pull && \
-rm -rf post static index.html && \
+rm -rf post static index.html styles.css && \
 cp -r dist/* . && \
-git add post static index.html && \
+git add post static index.html styles.css && \
 git commit -m "build: $(date)" &&\
 git push && \
 git checkout main
