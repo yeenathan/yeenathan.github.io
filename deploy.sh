435de1f1cdec8621497a4abbbf407e9a3c8a3e2f
yeenathan <yeenathan21@gmail.com>
2026-08-31 16:30:21 -0700

    fix deploy again for real final

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ git checkout gh-pages && \
 git pull && \
 find . -maxdepth 1 ! -path . ! -name .git ! -name dist -exec rm -rf {} + && \
 cp -r dist/* . && \
+rm -rf dist && \
 git add . && \
 git commit -m "build: $(date)" &&\
 git push && \
